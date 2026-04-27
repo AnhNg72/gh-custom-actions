63f2511ef11a98cd6666a9c2ffafe127e76701d8
Anh Ng <VanAnh.Nguyen@liebherr.com>
2026-04-27 15:01:19 +0200

    updated main.js code

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,8 +2,9 @@
 //const github = require('@actions/github')
 //const exec = require('@actions/exec')
 
-import core from '@actions/core';
-import github from '@actions/github';
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
 
 function run() {
     core.notice('Hello from my custom JavaScript Action!');
